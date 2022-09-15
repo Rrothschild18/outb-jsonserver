5aabfb25af44d523bc8b6a0f53a8e6e4baf89bb5
Raul Rothschild <raultonello18@gmail.com>
2022-09-15 00:11:39 -0300

    a mimir

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // server.js
-const express = require("express");
-const app = express();
 const jsonServer = require("json-server");
-const routes = jsonServer.router();
+const server = jsonServer.create();
+const router = jsonServer.router();
 const middlewares = jsonServer.defaults();
 
-app.use(routes);
-app.use(middlewares);
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
